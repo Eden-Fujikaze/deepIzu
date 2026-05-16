c8e9beac6014e3d1c1d5e9b0abe69c99956e8c40
Eden-Fujikaze <eden.fujikaze@gmail.com>
2026-05-16 17:10:03 +0200

    made talent map lazy to not crash if file doesnt exist

--- modules/talentMap.js
+++ modules/talentMap.js
@@ -1,4 +1,10 @@
-import { readFileSync } from 'fs';
-export const talentMap = Object.fromEntries(
-    JSON.parse(readFileSync('./talentList.json', 'utf-8')).map(t => [t.name, t])
-);
+import { readFileSync, existsSync } from 'fs';
+
+function loadTalentMap() {
+    if (!existsSync('./talentList.json')) return {};
+    return Object.fromEntries(
+        JSON.parse(readFileSync('./talentList.json', 'utf-8')).map(t => [t.name, t])
+    );
+}
+
+export const talentMap = loadTalentMap();
